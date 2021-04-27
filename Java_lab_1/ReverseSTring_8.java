392696d11897209f257fce85bb951eba8034dd3e
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-04-27 12:14:54 +0530

    Add files via upload

---
+++ Java_lab_1/ReverseSTring_8.java
@@ -0,0 +1,23 @@
+package lab.java.one.com;
+
+public class ReverseSTring_8 {
+
+	public static void main(String[] args) {
+		String input = "Full stack ";
+		 
+        // getBytes() method to convert string
+        // into bytes[].
+        byte[] strAsByteArray = input.getBytes();
+ 
+        byte[] result = new byte[strAsByteArray.length];
+ 
+        // Store result in reverse order into the
+        // result byte[]
+        for (int i = 0; i < strAsByteArray.length; i++)
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+ 
+        System.out.println(new String(result));
+
+	}
+
+}
